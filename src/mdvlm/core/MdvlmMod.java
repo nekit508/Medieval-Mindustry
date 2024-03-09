dc62ccdf6316207acb51952054e20bddbc2de54c
nekit508 <lastp508@gmail.com>
2024-03-09 22:09:09 +0300

    java integration: first step "project initialize"

---
+++ src/mdvlm/core/MdvlmMod.java
@@ -0,0 +1,17 @@
+package mdvlm.core;
+
+import mindustry.mod.Mod;
+import mma.annotations.ModAnnotations;
+
+@ModAnnotations.RootDirectoryPath(rootDirectoryPath = "")
+@ModAnnotations.AnnotationSettings(
+        rootPackage = "mdvlm",
+        modInfoPath = "res/mod.json",
+        classPrefix = "Mdvlm"
+)
+public class MdvlmMod extends Mod {
+    @Override
+    public void loadContent() {
+
+    }
+}
